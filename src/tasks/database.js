53a1088ea113663a4c67b2613ac4a88d1a715080
Erik Frerejean <erik@wedevelop.nl>
2023-10-31 11:33:54 +0100

    Provide a way to run `dev/buid` from a make command.

--- src/tasks/database.js
+++ src/tasks/database.js
@@ -3,8 +3,13 @@ import fetch from 'node-fetch';
 import util from 'util';
 const exec = util.promisify(child_process.exec);
 
-export const cypressReloadDBTask = async (baseUrl) => {
+export const cypressReloadDBTask = async (baseUrl, useFetch = true) => {
   await exec('make reload-database');
-  await fetch(`${baseUrl}/dev/build`);
+  if (useFetch) {
+    await fetch(`${baseUrl}/dev/build`);
+  }
+  else {
+    await exec('make devbuild');
+  }
   return null;
 }
